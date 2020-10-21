5a13c1ca0c8c1d8e8a2a3c77f301a5844f8dce41
gitbush <bush.matthewjames@gmail.com>
2020-10-21 21:35:23 +0100

    Import all revolve files
    Browser csp error

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,17 @@
 module.exports = function (eleventyConfig) {
   
     // set copy asset folder to dist
-    // eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPassthroughCopy('src/static');
   
     // set input and output folder
     return {
+      addPassthroughFileCopy: true,
+      markdownTemplateEngine: 'njk',
+      templateFormats: ['html', 'njk', 'md'],
       dir: { 
         input: 'src/site', 
         output: 'dist',
-        includes: '_inlcudes'
+        includes: 'includes'
     },
       dataTemplateEngine: 'njk',
       markdownTemplateEngine: 'njk'
